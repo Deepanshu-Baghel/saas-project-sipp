382d46b6b8d5301989e672ce61075b0d4c1996b1
Deepanshu Baghel <bagheldeepanshu7451@gmail.com>
2025-07-10 09:46:16 +0530

    login and register done

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react';
 import { HashRouter as RouteIt, Switch, Route } from 'react-router-dom';
 import Home from '../screens/home/Home';
-// import Home from '../screens/Home';
 import Layout from '../layout/Layout';
-
+import Login from '../screens/Login';
+import Register from '../screens/Register';
 
 class Router extends Component {
-    render() {
-        return (
+    state = {
+        loggedIn: false
+    };
 
+    handleLogin = () => {
+        this.setState({ loggedIn: true });
+    };
+
+    render() {
+        const { loggedIn } = this.state;
+
+        if (!loggedIn) {
+            return (
+                <RouteIt>
+                    <Switch>
+                        <Route exact path="/register" component={Register} />
+                        <Route path="/" render={(props) => <Login {...props} onLogin={this.handleLogin} />} />
+                    </Switch>
+                </RouteIt>
+            );
+        }
+
+        return (
             <RouteIt>
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route path="/student" component={Layout} />
-
                 </Switch>
             </RouteIt>
-
-        )
+        );
     }
 }
 
